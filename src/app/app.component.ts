b4050fd2ca99988c2116f95644169ee5169d5364
harshsonar <harshsonar40@gmail.com>
2024-03-12 17:30:43 +0530

    small change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { initializeApp } from '@angular/fire/app';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
 import { environment } from '../environments/environment';
 import { HomeComponent } from './home/home.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
-
-const firebaseApp = initializeApp(environment.firebaseConfig);
-const firebaseAuth = getAuth(firebaseApp);
 
 @Component({
   selector: 'app-root',
--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { initializeApp } from '@angular/fire/app';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
 import { RouterService } from './router.service';
+import { environment } from '../../environments/environment';
+
+
+const firebaseApp = initializeApp(environment.firebaseConfig);
+const firebaseAuth = getAuth(firebaseApp);
+
 
 @Injectable({
   providedIn: 'root'
